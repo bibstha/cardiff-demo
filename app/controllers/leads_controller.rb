ad586a8263b16e5b4a81349fbcfec0df17489fe2
Bibek Shrestha <bibekshrestha@gmail.com>
2025-05-12 13:39:42 -0700

    Onboarding and Personal Info view files.

--- app/controllers/leads_controller.rb
+++ app/controllers/leads_controller.rb
@@ -2,11 +2,21 @@ class LeadsController < ApplicationController
   include Wicked::Wizard
 
   steps :personal_info, :business_info, :review
+  before_action :set_progress, only: [:show, :update]
 
   layout 'onboarding'
 
+  def new
+    redirect_to leads_path
+  end
+
   def show
-    @lead = Lead.new
+    # Fetch the lead id from the session
+    if session[:lead_id].present?
+      @lead = Lead.find(session[:lead_id])
+    else
+      @lead = Lead.new
+    end
     render_wizard
   end
 
@@ -14,6 +24,7 @@ class LeadsController < ApplicationController
     @lead = Lead.new(lead_params)
 
     if @lead.save
+      session[:lead_id] = @lead.id
       redirect_to next_wizard_path
     else
       render_wizard
@@ -21,6 +32,14 @@ class LeadsController < ApplicationController
   end
 
   private
+
+  def set_progress
+    if wizard_steps.any? && wizard_steps.index(step).present?
+      @progress = ((wizard_steps.index(step) + 1).to_d / wizard_steps.count.to_d) * 100
+    else
+      @progress = 0
+    end
+  end
 
   def lead_params
     params.require(:lead).permit(
